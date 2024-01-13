1993c674bf8eb7936ce8352548dbfb18c77a6f3f
Mashentsev Danil <danil.mashentsev@mail.ru>
2024-01-13 23:32:32 +0300

    initial commit + added scss architecture + created checkbox component

---
+++ components/Checkbox.vue
@@ -0,0 +1,25 @@
+<template lang="">
+    <input type="checkbox" v-model='model' :value='value' :id='id' />
+    <label :for='id'>{{ label }}</label>
+</template>
+<script setup>
+    const props = defineProps({
+        modelValue: { type: [Array, Boolean] },
+        value: { type: [Boolean] },
+        label: { type: String },
+        id: { type: String },
+    })
+    const emit = defineEmits(['update:modelValue'])
+    const model = computed({
+        get() {
+            return props.modelValue
+        },
+        set(value) {
+            emit('update:modelValue', value)
+        }
+    })
+
+</script>
+<style lang="">
+    
+</style>
---
+++ pages/courses/index.vue
@@ -0,0 +1,11 @@
+<template lang="">
+    <div class="main-container">
+        <Checkbox value='1' label="asd" id='kek'/>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss">
+    
+</style>
