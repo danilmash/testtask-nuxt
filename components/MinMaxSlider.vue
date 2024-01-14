32cfa24821a6ebc2005e495fdee42788b96b60f3
Mashentsev Danil <danil.mashentsev@mail.ru>
2024-01-14 23:18:24 +0300

    added MinMaxSlider component

---
+++ components/MinMaxSlider.vue
@@ -0,0 +1,99 @@
+<template lang="">
+    <div class="minmax-slider" ref="slider">
+        <input class="min-slider"
+            type="range"
+            name="min"
+            id="min"
+            :min="minValue"
+            :max="maxValue"
+            :value="minValue"
+            :step="step"
+            @input="({ target }) => (sliderMinValue = parseFloat(target.value))"
+            />
+        <input class="max-slider"
+            type="range"
+            name="max"
+            id="max"
+            :min="minValue"
+            :max="maxValue"
+            :value="maxValue"
+            :step="step"
+            @input="({ target }) => (sliderMaxValue = parseFloat(target.value))"
+        />
+    </div>
+</template>
+<script setup>
+import { ref, watchEffect } from "vue";
+
+const props = defineProps({
+    step: {
+        type: Number,
+        default: 1,
+    },
+    minValue: {
+        type: Number,
+        default: 1,
+    },
+    maxValue: {
+        type: Number,
+        default: 100,
+    },
+    adjustment: {
+        type: Number,
+        default: 0,
+    }
+})
+
+const emit = defineEmits(['update:minValue', 'update:maxValue'])
+
+const sliderMinValue = ref(props.minValue)
+const sliderMaxValue = ref(props.maxValue)
+const slider = ref(null)
+
+const getPercent = (value, min, max) => {
+    return ((value - min) / (max - min)) * 100;
+};
+
+const setCSSProps = (width, left, right) => {
+    console.log(width)
+    if (100-right>left) {
+        slider.value.style.setProperty("--progressLeft", `${left}%`);
+        slider.value.style.setProperty("--progressRight", `${right}%`);
+    } else {
+        slider.value.style.setProperty("--sliderWidth", `${width}%`);
+        slider.value.style.setProperty("--progressLeft", `${100-right}%`);
+        slider.value.style.setProperty("--progressRight", `${left}%`);
+    }
+    
+};
+
+const sliderDifference = computed(() => {
+    if (sliderMinValue.value>sliderMaxValue.value) {
+            return Math.abs(sliderMinValue.value - sliderMaxValue.value + props.adjustment);
+        } else {
+            return Math.abs(sliderMaxValue.value - sliderMinValue.value + props.adjustment);
+        }
+
+ 
+    
+});
+
+watchEffect(() => {
+    if (slider.value) {
+        if (sliderMinValue.value>sliderMaxValue.value) {
+            emit('update:minValue', sliderMaxValue.value)
+            emit('update:maxValue', sliderMinValue.value)
+        } else {
+            emit("update:minValue", sliderMinValue.value);
+            emit("update:maxValue", sliderMaxValue.value);
+        }
+
+
+        const differencePercent = getPercent(sliderDifference.value, props.minValue, props.maxValue);
+        const leftPercent = getPercent(sliderMinValue.value, props.minValue, props.maxValue);
+        const rightPercent = 100 - getPercent(sliderMaxValue.value, props.minValue, props.maxValue);
+        setCSSProps(differencePercent, leftPercent, rightPercent);
+    }
+})
+
+</script>
--- pages/courses/index.vue
+++ pages/courses/index.vue
@@ -1,10 +1,11 @@
 <template lang="">
     <div class="main-container">
-        <Checkbox value='1' label="asd" id='kek'/>
+        <Checkbox :value='true' label="asd" id='kek'/>
+        <MinMaxSlider :minValue='1' :maxValue='6' :step="1" :adjustment="1"/>
     </div>
 </template>
 <script setup>
-
+    let qwe = ref(null)
 </script>
 <style lang="scss">
     
